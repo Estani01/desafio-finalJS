6cdfc24fcb91eaef3f0a0ef3044f68d273f62e69
estanislao01 <estani03@gmail.com>
2022-08-16 17:03:10 -0300

    commit final

---
+++ JS/carrito.js
@@ -0,0 +1,103 @@
+// div del carrito.
+let divCarrito = document.createElement("div");
+divCarrito.className = "carrito";
+divCarrito.id = "carritos";
+padreItem.append(divCarrito);
+
+
+//Definicion del Array carrito y manejo del DOM para crearlo.
+let carrito = [];
+let productosPrecio;
+let seccionCarrito = document.getElementById("carritos");
+
+
+let totalCompra = document.createElement("div");
+totalCompra.className = "card shadow p-3 mb-5 bg-body rounded cartaCarrito";
+totalCompra.innerHTML = "<h3>Total: </h3>"
+seccionCarrito.appendChild(totalCompra);
+
+
+let montoTotalCompra = document.createElement("h3");
+montoTotalCompra.innerText = "$0";
+totalCompra.appendChild(montoTotalCompra);
+
+
+let cantidadProductos = document.createElement("div");
+cantidadProductos.className = "card-body cuerpoCartaCarrito";
+cantidadProductos.innerHTML = `<h5 class="card-title">Cantidad de productos: </h5>`;
+totalCompra.appendChild(cantidadProductos);
+
+let cantProducts = document.createElement("h5");
+cantProducts.className = "card-text"
+cantProducts.innerText = "0";
+cantidadProductos.appendChild(cantProducts); 
+
+let botonesCarrito = document.createElement("div");
+botonesCarrito.className = "botoneraCarrito";
+cantidadProductos.appendChild(botonesCarrito);
+
+let botonComprar = document.createElement("div");
+botonComprar.innerHTML = `<button type="button" id="botonComprar" class="btn btn-warning">Comprar</button>`;
+botonesCarrito.appendChild(botonComprar); 
+
+let borrarCarrito = document.createElement("div");
+borrarCarrito.innerHTML = `<button type="button" id="borrarCarrito" class="btn btn-warning">Vaciar Carrito</button>`;
+botonesCarrito.appendChild(borrarCarrito); 
+
+
+
+
+
+//Vaciar Carrito.
+document.getElementById("borrarCarrito").onclick = () =>{
+    Swal.fire({
+        title: 'Esta seguro?',
+        text: "Esta accion no se puede revertir!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Vaciar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.clear();
+            carrito = [];
+            total = 0;
+            montoTotalCompra.innerText = "$" + total;
+            cantProducts.innerText = carrito.length;
+          Swal.fire(
+            'Vacio!',
+            'Se vacio el carrito de compras.',
+            'success'
+          )
+        }
+      })
+    
+};
+
+
+
+// funciones del carrito.
+function agregarAlCarrito (id){
+
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Su producto se agrego al carrito!',
+        showConfirmButton: false,
+        timer: 600
+      })
+            carrito.push(productosPrecio.find(p=>p.id==id));
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            calcularTotalCarrito();
+};
+
+function calcularTotalCarrito() {
+    let total = 0;
+    for (const productosPrecio of carrito) {
+        total+=productosPrecio.precio;
+    }
+    montoTotalCompra.innerText = "$" + total;
+    cantProducts.innerText = carrito.length;
+    
+};
---
+++ JS/funciones.js
@@ -0,0 +1,241 @@
+// funciones para crear las tarjetas trayendo los productos con fetch desde un json
+function crearTarjetasBoca(){
+    inicio.innerHTML = ("");
+    tarjetas.innerHTML = ("");
+    tarjetas.className = "tarjetas"
+    contenedorInicio.remove(container);
+    fetch("./data/data-bocajrs.json")
+    .then(resp =>resp.json())
+    .then(productos => {
+            productos.forEach(producto => {
+                let item = document.createElement("div");
+                item.className = "productos"
+                item.innerHTML = `  
+                                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                                    <a id="${producto.idImagen}" href="${producto.img2}" target="_blank"><img src="${producto.img}" class="card-img-top" alt="..."></a>
+                                    <div class="card-body">
+                                     <h5 class="card-title">$${producto.precio}</h5>
+                                     <p class="card-text">${producto.nombre}</p>
+                                     <button type="button" id="${producto.id}" class="btn btn-warning">Agregar al carrito</button>`;
+        
+                    tarjetas.append(item);
+                    padreItem.append(tarjetas)
+                    padreItem.append(divCarrito);
+                    document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+
+                    //CambiarImagenes.
+                        document.getElementById(`${producto.idImagen}`).onmousemove = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img2}" class="card-img-top" alt="...">`};
+                        document.getElementById(`${producto.idImagen}`).onmouseout = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img}" class="card-img-top" alt="...">`};
+
+                
+            })
+            productosPrecio = productos
+        
+        
+    }).catch(error => alert(`${error} No se pudo descargar la informacion. `));
+};
+function crearTarjetasRiver(){
+    inicio.innerHTML = ("");
+    tarjetas.innerHTML = ("");
+    tarjetas.className = "tarjetas";
+    contenedorInicio.remove(container);
+    fetch("./data/data-riverp.json")
+    .then(resp =>resp.json())
+    .then(productos => {
+            productos.forEach(producto => {
+                let item = document.createElement("div");
+                item.className = "productos"
+                item.innerHTML = `  
+                                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                                    <a id="${producto.idImagen}" href="${producto.img2}" target="_blank"><img src="${producto.img}" class="card-img-top" alt="..."></a>
+                                    <div class="card-body">
+                                     <h5 class="card-title">$${producto.precio}</h5>
+                                     <p class="card-text">${producto.nombre}</p>
+                                     <button type="button" id="${producto.id}" class="btn btn-warning">Agregar al carrito</button>`;
+        
+                                     tarjetas.append(item);
+                                     padreItem.append(tarjetas)
+                                     padreItem.append(divCarrito);
+                                     document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+
+                                     //CambiarImagenes.
+                                        document.getElementById(`${producto.idImagen}`).onmousemove = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img2}" class="card-img-top" alt="...">`};
+                                        document.getElementById(`${producto.idImagen}`).onmouseout = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img}" class="card-img-top" alt="...">`};
+
+                                     
+            })
+            productosPrecio = productos
+        
+        
+    }).catch(error => alert(`${error} No se pudo descargar la informacion. `));
+};
+function crearTarjetasArg(){
+    inicio.innerHTML = ("");
+    tarjetas.innerHTML = ("");
+    tarjetas.className = "tarjetas";
+    contenedorInicio.remove(container);
+    fetch("./data/data-arg.json")
+    .then(resp =>resp.json())
+    .then(productos => {
+            productos.forEach(producto => {
+                let item = document.createElement("div");
+                item.className = "productos"
+                item.innerHTML = `  
+                                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                                    <a id="${producto.idImagen}" href="${producto.img2}" target="_blank"><img src="${producto.img}" class="card-img-top" alt="..."></a>
+                                    <div class="card-body">
+                                     <h5 class="card-title">$${producto.precio}</h5>
+                                     <p class="card-text">${producto.nombre}</p>
+                                     <button type="button" id="${producto.id}" class="btn btn-warning">Agregar al carrito</button>`;
+        
+                                     tarjetas.append(item);
+                                     padreItem.append(tarjetas)
+                                     padreItem.append(divCarrito);
+                                     document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+
+                                     //CambiarImagenes.
+                                        document.getElementById(`${producto.idImagen}`).onmousemove = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img2}" class="card-img-top" alt="...">`};
+                                        document.getElementById(`${producto.idImagen}`).onmouseout = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img}" class="card-img-top" alt="...">`};
+
+                                     
+            })
+        
+            productosPrecio = productos
+        
+    }).catch(error => alert(`${error} No se pudo descargar la informacion. `));
+};
+    
+function crearTarjetasRestAmerica(){
+    inicio.innerHTML = ("");
+    tarjetas.innerHTML = ("");
+    tarjetas.className = "tarjetas"
+    contenedorInicio.remove(container);
+    fetch("./data/data-america.json")
+    .then(resp =>resp.json())
+    .then(productos => {
+            productos.forEach(producto => {
+                let item = document.createElement("div");
+                item.className = "productos"
+                item.innerHTML = `  
+                                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                                    <a id="${producto.idImagen}" href="${producto.img2}" target="_blank"><img src="${producto.img}" class="card-img-top" alt="..."></a>
+                                    <div class="card-body">
+                                     <h5 class="card-title">$${producto.precio}</h5>
+                                     <p class="card-text">${producto.nombre}</p>
+                                     <button type="button" id="${producto.id}" class="btn btn-warning">Agregar al carrito</button>`;
+        
+                    tarjetas.append(item);
+                    padreItem.append(tarjetas)
+                    padreItem.append(divCarrito);
+                    document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+
+                    //CambiarImagenes.
+                        document.getElementById(`${producto.idImagen}`).onmousemove = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img2}" class="card-img-top" alt="...">`};
+                        document.getElementById(`${producto.idImagen}`).onmouseout = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img}" class="card-img-top" alt="...">`};
+
+                
+            })
+        
+            productosPrecio = productos
+        
+    }).catch(error => alert(`${error} No se pudo descargar la informacion. `));
+};
+
+function crearTarjetasRestEuropa(){
+    inicio.innerHTML = ("");
+    tarjetas.innerHTML = ("");
+    tarjetas.className = "tarjetas"
+    contenedorInicio.remove(container);
+    fetch("./data/data-europa.json")
+    .then(resp =>resp.json())
+    .then(productos => {
+            productos.forEach(producto => {
+                let item = document.createElement("div");
+                item.className = "productos"
+                item.innerHTML = `  
+                                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                                    <a id="${producto.idImagen}" href="${producto.img2}" target="_blank"><img src="${producto.img}" class="card-img-top" alt="..."></a>
+                                    <div class="card-body">
+                                     <h5 class="card-title">$${producto.precio}</h5>
+                                     <p class="card-text">${producto.nombre}</p>
+                                     <button type="button" id="${producto.id}" class="btn btn-warning">Agregar al carrito</button>`;
+        
+                    tarjetas.append(item);
+                    padreItem.append(tarjetas)
+                    padreItem.append(divCarrito);
+                    
+                    document.getElementById(`${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+
+                    //CambiarImagenes.
+                        document.getElementById(`${producto.idImagen}`).onmousemove = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img2}" class="card-img-top" alt="...">`};
+                        document.getElementById(`${producto.idImagen}`).onmouseout = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img}" class="card-img-top" alt="...">`};
+
+                
+            })
+        
+            productosPrecio = productos
+        
+    }).catch(error => alert(`${error} No se pudo descargar la informacion. `));
+};
+
+function defaultIndex () {
+    tarjetas.innerHTML = ("");
+    tarjetas.className = "tarjetasInicio";
+    fetch("./data/data-index.json")
+    .then(resp =>resp.json())
+    .then(productos => {
+            productos.forEach(producto => {
+                let item = document.createElement("div");
+                item.className = "productos"
+                item.innerHTML = `
+                                    <div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+                                    <a id="${producto.idImagen}" href="${producto.img2}" target="_blank"><img src="${producto.img}" class="card-img-top" alt="..."></a>
+                                    <div class="card-body">
+                                     <h5 class="card-title">$${producto.precio}</h5>
+                                     <p class="card-text">${producto.nombre}</p>
+                                     <button type="button" id="${producto.id}" class="btn btn-warning">Ver Catalogo</button>`;
+        
+                                     tarjetas.append(item);
+                                     contenedorInicio.append(tarjetas)
+                                    padreItem.innerHTML=("")
+                                     document.getElementById(`${producto.id}`).onclick = () => {
+                                            if(producto.id===1){crearTarjetasArg()
+                                            }else if(producto.id===2){crearTarjetasBoca()
+                                            }else if(producto.id===3){
+                                                crearTarjetasRestEuropa()
+                                            }else{crearTarjetasArg()}
+                                     };
+
+                                     //CambiarImagenes.
+                                        document.getElementById(`${producto.idImagen}`).onmousemove = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img2}" class="card-img-top" alt="...">`};
+                                        document.getElementById(`${producto.idImagen}`).onmouseout = () =>{document.getElementById(`${producto.idImagen}`).innerHTML = `<img src="${producto.img}" class="card-img-top" alt="...">`};                                       
+
+            })
+    }).catch(error => alert(`${error} No se pudo descargar la informacion. `));
+};
+
+
+// botonera
+let clickBoca = document.getElementById("escudoboca").addEventListener("click", crearTarjetasBoca);
+let clickArg = document.getElementById("escudoarg").addEventListener("click", crearTarjetasArg);
+let clickRiver = document.getElementById("escudoriver").addEventListener("click", crearTarjetasRiver);
+let clickAmerica = document.getElementById("escudoamerica").addEventListener("click", crearTarjetasRestAmerica);
+let clickEuropa = document.getElementById("escudoeuropa").addEventListener("click", crearTarjetasRestEuropa);
+let clickHome = document.getElementById("botonHome");
+// condicional para cuando se apretan los distintos botones
+
+if (clickBoca) {
+    crearTarjetasBoca();
+}else if (clickArg){
+    crearTarjetasArg();
+}else if (clickRiver){
+    crearTarjetasRiver();
+}else if(clickAmerica){
+    crearTarjetasRestAmerica();
+}else if(clickEuropa){
+    crearTarjetasRestEuropa
+}else if(clickHome){
+    defaultIndex();
+}else{
+    defaultIndex();
+};
